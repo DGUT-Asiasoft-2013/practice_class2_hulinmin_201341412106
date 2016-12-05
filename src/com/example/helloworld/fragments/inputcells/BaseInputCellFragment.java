9d1d2f0d92829a50f56c90e1b155e49a013e0c04
Administrator <Administrator@7A304-0004>
2016-12-05 17:17:58 +0800

    first commit

---
+++ src/com/example/helloworld/fragments/inputcells/BaseInputCellFragment.java
@@ -0,0 +1,8 @@
+package com.example.helloworld.fragments.inputcells;
+
+import android.app.Fragment;
+
+public abstract class BaseInputCellFragment extends Fragment {
+	public abstract void setlabelText(String labelText);
+	public abstract void setHintText(String hintText);
+}
