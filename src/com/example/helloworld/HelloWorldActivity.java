65f201e279237889b00bce063d177640e7b2cab8
Administrator <Administrator@7A304-0004>
2016-12-07 14:32:35 +0800

    登录界面的四个按钮的初步设置和第一个按钮基本布局的初步设置

--- src/com/example/helloworld/HelloWorldActivity.java
+++ src/com/example/helloworld/HelloWorldActivity.java
@@ -1,15 +1,71 @@
 package com.example.helloworld;
 
+import com.example.helloworld.fragments.pages.FeedListFragment;
+import com.example.helloworld.fragments.pages.MyProfileFragment;
+import com.example.helloworld.fragments.pages.NoteListFragment;
+import com.example.helloworld.fragments.pages.SearchPageFragment;
+import com.example.helloworld.fragments.widgets.MainTabbarFragment;
+import com.example.helloworld.fragments.widgets.MainTabbarFragment.OnTabSelectedListener;
+
 import android.app.Activity;
+import android.app.Fragment;
 import android.os.Bundle;
 
 public class HelloWorldActivity extends Activity {
-	@Override
+
+	FeedListFragment contentFeedList = new FeedListFragment();
+	NoteListFragment contentNoteList = new NoteListFragment();
+	SearchPageFragment contentSearchPage = new SearchPageFragment();
+	MyProfileFragment contentMyProfile = new MyProfileFragment();
+
+	MainTabbarFragment tabber;
+
+
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
+
 		super.onCreate(savedInstanceState);
-		
+
 		setContentView(R.layout.activity_helloworld);
+
+		tabber = (MainTabbarFragment) getFragmentManager().findFragmentById(R.id.frag_tabbar);
+		tabber.setOnTabSelectedListener(new OnTabSelectedListener() {
+
+			@Override
+			public void onTabSelected(int index) {
+				changeContentFragment(index);
+
+			}
+		});
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+
+		tabber.setSelectedItem(0);
+	}
+
+	void changeContentFragment(int index){
+		Fragment newFrag = null;
+
+		switch (index){
+		case 0:newFrag = contentFeedList; break;
+		case 1:newFrag = contentNoteList; break;
+		case 2:newFrag = contentSearchPage;break;
+		case 3:newFrag = contentMyProfile;break;
+
+		default:
+			break;
+
+		}
+
+		if(newFrag==null) return;
+
+		getFragmentManager()
+		.beginTransaction()
+		.replace(R.id.content, newFrag)
+		.commit();
 	}
 
 }
