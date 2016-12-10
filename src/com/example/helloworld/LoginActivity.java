59491473783c2313965a005b5dab69d945006e88
Administrator <Administrator@7A304-0004>
2016-12-10 14:33:10 +0800

    登录界面的数据库后台实现及显示

--- src/com/example/helloworld/LoginActivity.java
+++ src/com/example/helloworld/LoginActivity.java
@@ -1,11 +1,26 @@
 package com.example.helloworld;
 
+import java.io.IOException;
+
+import com.example.helloworld.entity.User;
 import com.example.helloworld.fragments.inputcells.SimpleTextInputCellFragment;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class LoginActivity extends Activity {
 	SimpleTextInputCellFragment fragAccount,fragPassword;
@@ -13,6 +28,9 @@ public class LoginActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
+		
+
+		
 		
 		findViewById(R.id.btn_register).setOnClickListener(new View.OnClickListener() {
 			
@@ -64,12 +82,114 @@ public class LoginActivity extends Activity {
 	}
 	
 	void goLogin(){
-		Intent itnt = new Intent(this,HelloWorldActivity.class);
-		startActivity(itnt);
+//		Intent itnt = new Intent(this,HelloWorldActivity.class);
+//		startActivity(itnt);
+		
+		
+		String account = fragAccount.getText();
+		String password = fragPassword.getText();
+
+		
+
+		password = MD5.getMD5(password);
+
+
+
+		OkHttpClient client=new OkHttpClient();
+
+		MultipartBody.Builder requestBodyBuilder = new MultipartBody.Builder()
+				.setType(MultipartBody.FORM)
+				.addFormDataPart("account", account)
+				
+				.addFormDataPart("passwordHash", password);
+
+		
+
+
+		Request request=new Request.Builder()
+				.url("http://172.27.0.22:8080/membercenter/api/login")
+				.method("post", null)
+				.post(requestBodyBuilder.build())
+				.build();
+
+		final ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this);
+		progressDialog.setMessage("ÇëÉÔºò");
+		progressDialog.setCancelable(false);
+		progressDialog.setCanceledOnTouchOutside(false);
+
+		client.newCall(request).enqueue(new Callback() {
+			
+
+			@Override
+			public void onResponse(final Call arg0,final Response arg1) throws IOException {
+				
+				final String responseString = arg1.body().string();
+				ObjectMapper mapper = new ObjectMapper();
+				final User user = mapper.readValue(responseString, User.class);
+				
+				runOnUiThread(new Runnable() {
+
+					@Override
+					public void run() {
+						progressDialog.dismiss();
+
+						new AlertDialog.Builder(LoginActivity.this)
+						.setTitle("µÇÂ¼³É¹¦")
+						.setMessage("Hello,"+user.getName())
+//						.setMessage(responseBody)
+						.setPositiveButton("ºÃ", new DialogInterface.OnClickListener() {
+
+							@Override
+							public void onClick(DialogInterface dialog, int which) {
+								goHelloWord();
+
+							}
+						})
+						.show();
+					}
+				});
+
+			}
+
+			@Override
+			public void onFailure(final Call arg0, final IOException arg1) {
+				runOnUiThread(new Runnable() {
+
+					@Override
+					public void run() {
+						progressDialog.dismiss();
+
+						LoginActivity.this.onFailure(arg0, arg1);
+
+					}
+				});
+
+			}
+		});
+		
+		
+		
+		
 	}
 	
 	void goRecoverPassword(){
 		Intent itnt = new Intent(this,PasswordRecoverActivity.class);
 		startActivity(itnt);
+	}
+	
+	
+
+	void onFailure(Call arg0, Exception arg1) {
+		new AlertDialog.Builder(this)
+		.setTitle("µÇÂ¼Ê§°Ü")
+		.setMessage(arg1.getLocalizedMessage())
+		.setNegativeButton("ºÃ", null)
+		.show();
+
+	}
+	
+	void goHelloWord(){
+	Intent itnt = new Intent(this,HelloWorldActivity.class);
+	startActivity(itnt);
 	}
 }
